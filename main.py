ec2cabb406250b93d6bdef4d9691cad572590bd5
Ro Flam <romina.flam@yahoo.com>
2023-11-17 16:15:36 +0100

    Adding initial flask example

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask.json import jsonify
+
+app = Flask(__name__)  # Flask("main.py") --> INSTANCIRANJE FLASK OBJEKTA
+
+@app.route("/") # --> ISHODISNA RUTA A LA GOOGLE.CO; NASA JE http://127.0.0.1--> LOCALHOST
+def hello_world():
+    return "<P>Hello World</p>"
+
+
+
+# dict_primjer = {
+#     "Ime": "Marko",
+#     "Prezime": "Markic",
+#     "Email": "marko@mail.com"
+# }
+
+# @app.route("/")
+# def home():
+#     return "<h1><a href='/about'>Home page</a></h1>"
+
+# @app.route("/about")
+# def about():
+#     return "<p>About page</p>"
+
+# @app.route("/user/<username>")
+# def user(username):
+#     return f"<h3>User page for: {username}</h3>"
+
+# @app.route("/json")
+# def json():
+#     return jsonify(dict_primjer)
+
+# @app.route("/json/<key>")
+# def json_value(key):
+#     return dict_primjer.get(key, "Unknown key")
+
